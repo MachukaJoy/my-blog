7ae9e9366022842b452174646f38af8faf2a3cc4
Machuka Joy <machukajoy@gmail.com>
2022-05-17 20:55:21 +0300

    ft(class): add forms to upload comments and blogs

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,3 +5,12 @@ from wtforms.validators import InputRequired
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Bio.',validators = [InputRequired()])
     submit = SubmitField('Submit')
+
+class BlogForm(FlaskForm):
+    title_blog = StringField('Title')
+    description = TextAreaField('Write a Description', validators=[InputRequired()])
+    submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Add a comment', validators=[InputRequired()])
+    submit = SubmitField('Comment')
