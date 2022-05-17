91168797371f3c661f001adedbe5cc536e944a63
Machuka Joy <machukajoy@gmail.com>
2022-05-17 17:40:01 +0300

    ft: add authentication functions

---
+++ app/main/views.py
@@ -0,0 +1,11 @@
+from flask import render_template,request,redirect,url_for,abort
+from . import main
+from flask_login import login_required
+
+
+# Views
+@main.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
